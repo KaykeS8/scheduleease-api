4060f2de0be3e4ac69c91f7714f6c9c413ca37d7
Kayke simao <kayke.py@gmail.com>
2026-06-17 19:48:02 -0300

    fix: Unnecessaray imports removed

--- src/main/java/simao/project/agendamento/dtos/professional/ProfessionalResponseDto.java
+++ src/main/java/simao/project/agendamento/dtos/professional/ProfessionalResponseDto.java
@@ -1,9 +1,5 @@
 package simao.project.agendamento.dtos.professional;
 
-import simao.project.agendamento.entitys.Scheduling;
-
-import java.util.List;
-
 public record ProfessionalResponseDto(
         Long id,
         String name,
