9371c599475f2aaa76a349a0513dad76cd4c5b34
Kayke simao <kayke.py@gmail.com>
2026-06-17 20:47:19 -0300

    feat: added validate @email in field email

--- src/main/java/simao/project/agendamento/dtos/client/ClientRequestDto.java
+++ src/main/java/simao/project/agendamento/dtos/client/ClientRequestDto.java
@@ -1,10 +1,11 @@
 package simao.project.agendamento.dtos.client;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record ClientRequestDto(
     @NotBlank String name,
-    @NotBlank String email,
+    @NotBlank @Email String email,
     @NotBlank String phone
 ){
 }
