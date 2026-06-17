a109f5d9e5b3db2f17ec4a4fba50efd7ec5d4dc6
Kayke simao <kayke.py@gmail.com>
2026-06-17 19:45:47 -0300

    feat: Finished CRUD operation of scheduling controller

--- src/main/java/simao/project/agendamento/controllers/SchedulingController.java
+++ src/main/java/simao/project/agendamento/controllers/SchedulingController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import simao.project.agendamento.dtos.scheduling.SchedulingRequestDto;
 import simao.project.agendamento.dtos.scheduling.SchedulingResponseDto;
+import simao.project.agendamento.dtos.scheduling.SchedulingUpdateDto;
 import simao.project.agendamento.services.SchedulingService;
 
 import java.util.List;
@@ -29,4 +30,21 @@ public class SchedulingController {
         return ResponseEntity.status(HttpStatus.OK).body(schedulingService.createScheduling(schedulingRequestDto));
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<SchedulingResponseDto> getSchedulingById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(schedulingService.getSchedulingById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SchedulingResponseDto> updateScheduling(@RequestBody @Valid SchedulingUpdateDto schedulingUpdateDto, @PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(schedulingService.updateScheduling(schedulingUpdateDto, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteScheduling(@PathVariable Long id) {
+        schedulingService.deleteScheduling(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
 }
+
