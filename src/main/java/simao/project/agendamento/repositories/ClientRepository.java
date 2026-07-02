aa8691b602bf36f883fe8937b5d5ce942afb6ebd
Kayke simao <kayke.py@gmail.com>
2026-07-01 21:03:47 -0300

    Feat: method to find client by email add

--- src/main/java/simao/project/agendamento/repositories/ClientRepository.java
+++ src/main/java/simao/project/agendamento/repositories/ClientRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import simao.project.agendamento.entitys.Client;
 
+import java.util.Optional;
+
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findClientByEmail(String email);
 }
